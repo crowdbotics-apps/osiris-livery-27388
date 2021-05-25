dc0974933891186ccb5d7974ecab304480655d5d
Crowdbotics <team@crowdbotics.com>
2021-05-25 18:44:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/osiris_livery_27388/MainActivity.java
@@ -0,0 +1,15 @@
+package com.osiris_livery_27388;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "osiris_livery_27388";
+  }
+}
---
+++ backend/osiris_livery_27388/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for osiris_livery_27388 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'osiris_livery_27388.settings')
+
+application = get_wsgi_application()
